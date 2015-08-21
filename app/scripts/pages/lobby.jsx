32551f6be560d27cd03641f7694c34768aaaa064
Devin McGinty <devin.lee.mcginty@gmail.com>
2015-08-21 14:22:21 -0400

    Add structure for auth validation

--- app/scripts/pages/lobby.jsx
+++ app/scripts/pages/lobby.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 import GameList from '../components/GameList.jsx';
 
 import LobbyActions from '../actions/LobbyActions';
@@ -6,7 +7,23 @@ import LobbyStore from '../stores/LobbyStore';
 
 class Lobby extends React.Component {
 
+  validate() {
+
+    var redirect = '/auth/login?redirect=' + encodeURIComponent(window.location);
+
+    $.ajax({
+      type: 'GET',
+      url: '/auth/me',
+      statusCode: {
+        401: function() { window.location = redirect; }
+      }
+    });
+
+  }
+
   render() {
+    //this.validate();
+
     return (
         <div>
           <GameList />
