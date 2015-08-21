f7e9d1406e45ece0b399140ea75f1ad3d65c6c51
Devin McGinty <devin.lee.mcginty@gmail.com>
2015-08-21 16:18:46 -0400

    Pieces show up on a board

--- app/scripts/components/BoardSquare.jsx
+++ app/scripts/components/BoardSquare.jsx
@@ -3,7 +3,65 @@ import React from 'react';
 class BoardSquare extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {color: props.black};
+    this.state = {
+      color: props.black,
+      piece: props.piece
+    };
+  }
+
+  render_piece() {
+    var img = '../../images/';
+
+    switch (this.props.piece) {
+      // White Pieces
+      case 'K':
+        img += 'wK.svg';
+        break;
+      case 'Q':
+        img += 'wQ.svg';
+        break;
+      case 'R':
+        img += 'wR.svg';
+        break;
+      case 'B':
+        img += 'wB.svg';
+        break;
+      case 'N':
+        img += 'wN.svg';
+        break;
+      case 'P':
+        img += 'wP.svg';
+        break;
+      // Black Pieces
+      case 'k':
+        img += 'bK.svg';
+        break;
+      case 'q':
+        img += 'bQ.svg';
+        break;
+      case 'r':
+        img += 'bR.svg';
+        break;
+      case 'b':
+        img += 'bB.svg';
+        break;
+      case 'n':
+        img += 'bN.svg';
+        break;
+      case 'p':
+        img += 'bP.svg';
+        break;
+
+      default:
+        return '';
+        break;
+    }
+
+    return (
+            <img style={{maxHeight: "100%",
+                        maxWidth: "100%"}}
+                src={img} />
+           );
   }
 
   render() {
@@ -12,7 +70,7 @@ class BoardSquare extends React.Component {
            className="boardsquare">
         <div className="square_align">
           <div className="inner_square">
-            {this.props.children}
+            { this.render_piece() }
           </div>
         </div>
       </div>
--- app/scripts/components/GameChessBoard.jsx
+++ app/scripts/components/GameChessBoard.jsx
@@ -2,26 +2,60 @@ import React from 'react';
 import BoardSquare from './BoardSquare.jsx';
 
 class GameChessBoard extends React.Component {
-  render_square(pos) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    }
+  }
+
+  render_square(pos, piece) {
     var file = pos % 8;
     var rank = Math.floor(pos / 8);
     var black = (rank + file) % 2;
     return (
       <div key={pos} className="square_wrapper">
-        <BoardSquare black={black}>
+        <BoardSquare black={black} piece={piece}>
         </BoardSquare>
       </div>
     );
   }
 
-  render() {
+  render_row(fen, row_num) {
     var squares = [];
-    for (var i = 0; i < 64; i++) {
-      squares.push(this.render_square(i));
+
+    for (var i = 0; i < fen.length; i++) {
+      var pos = (row_num * 8) + i;
+
+      if (isNaN(fen[i])) {
+        squares.push(this.render_square(pos, fen[i]));
+      }
+      else {
+        var blank = parseInt(fen[i]);
+        for (; i < blank; i++, pos++) {
+          squares.push(this.render_square(pos, '-'));
+        }
+      }
     }
+
+    return squares;
+  }
+
+  squares() {
+    var squares = [],
+        fen_rows = this.state.fen.split(' ')[0].split('/');
+
+    for (var row = 0; row < fen_rows.length; row += 1) {
+      squares.push(this.render_row(fen_rows[row], row));
+    }
+
+    return squares;
+ }
+
+  render() {
     return (
         <div className="chessboard">
-          {squares}
+          {this.squares()}
         </div>
       )
   }
