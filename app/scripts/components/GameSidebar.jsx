8633ba2fadba1f6b749de9b546715add90ab3bf2
Devin McGinty <devin.lee.mcginty@gmail.com>
2015-08-23 10:00:16 -0400

    Add check for active player for game sidebar

--- app/scripts/components/GameSidebar.jsx
+++ app/scripts/components/GameSidebar.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import GameNavigation from './GameNavigation.jsx';
+import GameStore from '../stores/GameStore';
 
 class GameSidebar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fen: GameStore.getGameFEN()
+    };
+  }
+
+  activePlayer() {
+    var active = this.state.fen.split(' ')[1];
+    return active === 'w' ? "White to play" : "Black to play";
+  }
+
   render() {
     return (
       <div className="panel panel-info">
         <div className="panel-heading" style={{textAlign: 'center'}}>
-          <h4>It is currently your turn.</h4>
+          <h3>
+            { this.activePlayer() }
+          </h3>
         </div>
         <div className="panel-body">
           <div className="row-fluid">
